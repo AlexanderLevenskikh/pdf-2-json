e2ca2c032241b185b8ec46dff9a0c88fb1cdbad3
Alexander Levenskikh <a.levenskikh@gmail.com>
2020-04-16 12:20:14 +0300

    Add .env in back

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app';
+import * as path from "path";
+
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(process.env.APP_PORT || 5000);
 }
 bootstrap();
